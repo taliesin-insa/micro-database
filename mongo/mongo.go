5e085ba3dd8bcbdf7446460b8f6e5d093ae44350
Hummel Emilie <emilie.hummel@insa-rennes.fr>
2019-12-03 15:05:52 +0100

    First commit of mongo api

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -11,27 +11,58 @@ import (
 )
 
 // You will be using this Trainer type later in the program
-type Trainer struct {
-	Name string
-	Age  int
-	City string
+type Picture struct {
+	Id int
+	Type string
+	Value  string
+	Zone string
+	Children string
+	Parent string
+	Url string
+	Annotated bool
+	Corrected bool
+	SentToReco bool
+	SentToUser bool
+	Unreadable bool
 }
 
-func insertOne(b []byte, collection *mongo.Collection) {
-	var trainer Trainer
-	err := json.Unmarshal(b, &trainer)
+type Modification struct {
+	Id int
+	Flag string
+	Value bool
+}
 
-	insertResult, err := collection.InsertOne(context.TODO(), trainer)
+type Annotation struct {
+	Id int
+	Value string
+}
+
+/**
+	From a json flow, insert one entry in the database -> Useless
+	TODO : vérification des champs avant l'insertion
+ */
+func InsertOne(b []byte, collection *mongo.Collection) {
+	var pic Picture
+	err := json.Unmarshal(b, &pic)
 	if err != nil {
 		log.Fatal(err)
 	}
+	insertResult, err := collection.InsertOne(context.TODO(), pic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
 }
 
-func insertMany(b []byte, collection *mongo.Collection) {
-	var trainers []interface{}
-	err := json.Unmarshal(b, &trainers)
-	insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
+/**
+From a json flow, insert multiple entries in the database
+TODO : vérification des champs avant l'insertion
+*/
+func InsertMany(b []byte, collection *mongo.Collection) {
+	var pics []interface{}
+	err := json.Unmarshal(b, &pics)
+	insertManyResult, err := collection.InsertMany(context.TODO(), pics)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,9 +70,9 @@ func insertMany(b []byte, collection *mongo.Collection) {
 	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
 }
 
-func findOne(key, value string, collection *mongo.Collection) Trainer{
+func FindOne(key string, value int, collection *mongo.Collection) Picture{
 	filter := bson.D{{key, value}}
-	var result Trainer
+	var result Picture
 
 	err := collection.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
@@ -52,14 +83,14 @@ func findOne(key, value string, collection *mongo.Collection) Trainer{
 	return result
 }
 
-func findMany(key, value string, collection *mongo.Collection) []Trainer {
+func FindMany(key string, value int, collection *mongo.Collection) []Picture {
 
 	// Pass these options to the Find method
 	findOptions := options.Find()
 	//findOptions.SetLimit(2)
 
 	// Here's an array in which you can store the decoded documents
-	var results []Trainer
+	var results []Picture
 
 	// Passing bson.D{{}} as the filter matches all documents in the collection
 	cur, err := collection.Find(context.TODO(), bson.D{{key, value}}, findOptions)
@@ -72,7 +103,7 @@ func findMany(key, value string, collection *mongo.Collection) []Trainer {
 	for cur.Next(context.TODO()) {
 
 		// create a value into which the single document can be decoded
-		var elem Trainer
+		var elem Picture
 		err := cur.Decode(&elem)
 		if err != nil {
 			log.Println(err)
@@ -92,7 +123,62 @@ func findMany(key, value string, collection *mongo.Collection) []Trainer {
 	return results
 }
 
-func deleteAll(collection *mongo.Collection) {
+/**
+	Modify the différents flags
+ */
+func UpdateFlags(b []byte, collection *mongo.Collection) {
+	var modifications []Modification
+	var filter, update bson.D
+	err := json.Unmarshal(b,&modifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	for _, modif := range modifications {
+		filter = bson.D{{"Id",modif.Id}}
+		update = bson.D{
+			{"$set", bson.D{
+				{modif.Flag, modif.Value},
+			}},
+		}
+		updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+	}
+}
+
+/**
+	Annote multiple documents. Take a JSON comporting the id of annotated documents and the value of annotation text.
+	Set the annotated flag to true
+ */
+func UpdateValue(b []byte, collection *mongo.Collection) {
+	var annotations []Annotation
+	var filter, update bson.D
+	err := json.Unmarshal(b,&annotations)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, annot := range annotations {
+		filter = bson.D{{"Id",annot.Id}}
+		update = bson.D{
+			{"$set", bson.D{
+				{"Value", annot.Value},
+				{"Annotated", true},
+			}},
+		}
+		updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+	}
+}
+
+func DeleteAll(collection *mongo.Collection) {
 	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{{}})
 	if err != nil {
 		log.Fatal(err)
@@ -100,7 +186,7 @@ func deleteAll(collection *mongo.Collection) {
 	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
 }
 
-func deleteOne(key, value string, collection *mongo.Collection) {
+func DeleteOne(key string, value int, collection *mongo.Collection) {
 	filter := bson.D{{key, value}}
 
 	del, err := collection.DeleteOne(context.TODO(), filter)
@@ -111,7 +197,7 @@ func deleteOne(key, value string, collection *mongo.Collection) {
 	}
 }
 
-func disconnect(client *mongo.Client) {
+func Disconnect(client *mongo.Client) {
 	//Disconnection
 	err := client.Disconnect(context.TODO())
 
@@ -121,7 +207,7 @@ func disconnect(client *mongo.Client) {
 	fmt.Println("Connection to MongoDB closed.")
 }
 
-func connect() *mongo.Client{
+func Connect() *mongo.Client{
 	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
@@ -145,54 +231,57 @@ func connect() *mongo.Client{
 }
 
 func main() {
-	client := connect()
+	client := Connect()
 
 	//access the DB
-	collection := client.Database("test").Collection("trainers")
+	collection := client.Database("test").Collection("Docs")
 
 	//create some trainers
-	ash := Trainer{"Ash", 10, "Pallet Town"}
-	misty := Trainer{"Misty", 10, "Cerulean City"}
-	brock := Trainer{"Brock", 15, "Pewter City"}
-	gregre := Trainer{"Gregre", 21, "Pewter City"}
+	doc0 := Picture{0, "","","","","","",false,false,false,false, false}
+	doc1 := Picture{1, "","","","","","",false,false,false,false, false}
+	doc2 := Picture{2, "","","","","","",false,false,false,false, false}
+	doc3 := Picture{3, "","","","","","",false,false,false,false, false}
+	doc4 := `[{"Id": 4, "Type": "", "Value": "", "Children": "", "Parent": "", "Url" : "", "Annotated":false}]`	//JSON with holes
 
 	//Test
 	fmt.Println("TEST")
-	b, err := json.Marshal(ash)
-	insertOne(b, collection)
+	b, _ := json.Marshal(doc0)
+	InsertOne(b, collection)
 
 	//insert multiple entries
-	trainers := []interface{}{misty, brock, gregre}
-	bs, err := json.Marshal(trainers)
-	insertMany(bs, collection)
+	docs := []interface{}{doc1, doc2, doc3}
+	bs, _ := json.Marshal(docs)
+	InsertMany(bs, collection)
 
-	findOne("name", "Ash", collection)		// why does name need to be in lowercase for Ash ???
+	InsertMany([]byte(doc4), collection)
 
-	findOne("Name","Misty", collection)
+	FindOne("id", 0, collection) // WHY does name need to be in lowercase for the first one ???
 
-	findMany("City", "Pewter City", collection)
+	FindOne("Id",1, collection)
 
-	deleteOne("Name", "Gregre", collection)
+	FindMany("Id",4, collection)
 
-	findMany("City", "Pewter City", collection)
+	DeleteOne("Id", 3, collection)
 
-	//prepares the update of one document (using bson)
-	filter := bson.D{{"name", "Ash"}}
+	//FindMany("Value", "", collection)
 
-	update := bson.D{
-		{"$set", bson.D{
-			{"age", 12},
-		}},
-	}
 
-	//actually update
-	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+	modification := `[{"Id": 2,"Flag": "Annotated","Value": true}]`
 
-	deleteAll(collection)
+	modif := []byte(modification)
 
-	disconnect(client)
+	UpdateFlags(modif, collection)
+
+	FindOne("Id", 2, collection)
+
+	modification = `[{"Id": 1,"Value": "This text is annotated"}]`
+	modif = []byte(modification)
+
+	UpdateValue(modif, collection)
+
+	FindOne("Id", 1, collection)
+
+	DeleteAll(collection)
+
+	Disconnect(client)
 }
