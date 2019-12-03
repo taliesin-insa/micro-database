574a84c70bda6c576776cbf19fd422e89bbf2958
Hummel Emilie <emilie.hummel@insa-rennes.fr>
2019-12-03 15:07:16 +0100

    First commit of rest api

---
+++ rest/rest-api.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	mongogo "MongoGo/mongo"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var Client = mongogo.Connect()
+var Collection = Client.Database("test").Collection("trainers")
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func createEntry(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	mongogo.InsertMany(reqBody,Collection)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+/**
+	TODO : not working
+ */
+func updateFlags(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	mongogo.UpdateFlags(reqBody,Collection)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func updateValue(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	mongogo.UpdateValue(reqBody, Collection)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+
+//func getOneEvent(w http.ResponseWriter, r *http.Request) {
+//	eventID := mux.Vars(r)["id"]
+//
+//	for _, singleEvent := range events {
+//		if singleEvent.ID == eventID {
+//			json.NewEncoder(w).Encode(singleEvent)
+//		}
+//	}
+//}
+//
+//func getAllEvents(w http.ResponseWriter, r *http.Request) {
+//	json.NewEncoder(w).Encode(events)
+//}
+//
+//func updateEvent(w http.ResponseWriter, r *http.Request) {
+//	eventID := mux.Vars(r)["id"]
+//	var updatedEvent event
+//
+//	reqBody, err := ioutil.ReadAll(r.Body)
+//	if err != nil {
+//		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+//	}
+//	json.Unmarshal(reqBody, &updatedEvent)
+//
+//	for i, singleEvent := range events {
+//		if singleEvent.ID == eventID {
+//			singleEvent.Title = updatedEvent.Title
+//			singleEvent.Description = updatedEvent.Description
+//			events = append(events[:i], singleEvent)
+//			json.NewEncoder(w).Encode(singleEvent)
+//		}
+//	}
+//}
+//
+//func deleteEvent(w http.ResponseWriter, r *http.Request) {
+//	eventID := mux.Vars(r)["id"]
+//
+//	for i, singleEvent := range events {
+//		if singleEvent.ID == eventID {
+//			events = append(events[:i], events[i+1:]...)
+//			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+//		}
+//	}
+//}
+
+func main() {
+
+	// Define the routing
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/insert", createEntry).Methods("POST")
+
+	router.HandleFunc("/update/flags", updateFlags).Methods("PUT")
+	router.HandleFunc("/update/value/user", updateValue).Methods("PUT")
+
+	//router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	//router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	//router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
