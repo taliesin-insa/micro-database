8d8dc58a887e0b18649b510a36b90e771a51a095
nk-fouque <fouquen@outlook.fr>
2020-03-03 13:03:04 +0100

    refactor(RestAPI): Deleting old comments with example functions

--- src/micro-database/rest-api.go
+++ src/micro-database/rest-api.go
@@ -155,51 +155,6 @@ func deleteAll(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusAccepted)
 }
 
-//func getOneEvent(w http.ResponseWriter, r *http.Request) {
-//	eventID := mux.Vars(r)["id"]
-//
-//	for _, singleEvent := range events {
-//		if singleEvent.ID == eventID {
-//			json.NewEncoder(w).Encode(singleEvent)
-//		}
-//	}
-//}
-//
-//func getAllEvents(w http.ResponseWriter, r *http.Request) {
-//	json.NewEncoder(w).Encode(events)
-//}
-//
-//func updateEvent(w http.ResponseWriter, r *http.Request) {
-//	eventID := mux.Vars(r)["id"]
-//	var updatedEvent event
-//
-//	reqBody, err := ioutil.ReadAll(r.Body)
-//	if err != nil {
-//		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-//	}
-//	json.Unmarshal(reqBody, &updatedEvent)
-//
-//	for i, singleEvent := range events {
-//		if singleEvent.ID == eventID {
-//			singleEvent.Title = updatedEvent.Title
-//			singleEvent.Description = updatedEvent.Description
-//			events = append(events[:i], singleEvent)
-//			json.NewEncoder(w).Encode(singleEvent)
-//		}
-//	}
-//}
-//
-//func deleteEvent(w http.ResponseWriter, r *http.Request) {
-//	eventID := mux.Vars(r)["id"]
-//
-//	for i, singleEvent := range events {
-//		if singleEvent.ID == eventID {
-//			events = append(events[:i], events[i+1:]...)
-//			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
-//		}
-//	}
-//}
-
 // Actual API
 func main() {
 
@@ -218,8 +173,5 @@ func main() {
 
 	router.HandleFunc("/db/delete/all", deleteAll).Methods("PUT")
 
-	//router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	//router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
-	//router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
