cc13fe60bd80114befa06e8fd328dbd4555152b4
Nicolas Fouque <nicolas.fouque@insa-rennes.fr>
2020-04-12 11:09:36 +0000

    Old newPage was useless, re routed /retrieve/snippets to newPageWithSuggestions

--- src/micro-database/rest-api.go
+++ src/micro-database/rest-api.go
@@ -84,42 +84,6 @@ func selectById(w http.ResponseWriter, r *http.Request) {
 	w.Write(body)
 }
 
-func newPage(w http.ResponseWriter, r *http.Request) {
-	entryAmnt := mux.Vars(r)["amount"]
-	amount, err := strconv.Atoi(entryAmnt)
-	if err != nil {
-		log.Printf("[ERROR] : %v", err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("[MICRO-DATABASE] %v", err.Error())))
-	}
-
-	entry, err := FindManyUnused(amount, Database)
-	if err != nil {
-		log.Printf("[ERROR] : %v", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("[MICRO-DATABASE] %v", err.Error())))
-		return
-	}
-
-	if len(entry) < amount {
-		unsused, err := FindManyWithSuggestion(amount-len(entry), Database)
-		if err != nil {
-			log.Printf("[ERROR] : %v", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(fmt.Sprintf("[MICRO-DATABASE] %v", err.Error())))
-			return
-		}
-		for _, pic := range unsused {
-			entry = append(entry, pic)
-		}
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	body, _ := json.Marshal(entry)
-	w.WriteHeader(http.StatusOK)
-	w.Write(body)
-}
-
 func newPageWithSuggestions(w http.ResponseWriter, r *http.Request) {
 	entryAmnt := mux.Vars(r)["amount"]
 	amount, err := strconv.Atoi(entryAmnt)
@@ -323,8 +287,7 @@ func main() {
 
 	router.HandleFunc("/db/select/{id}", selectById).Methods("GET")
 	router.HandleFunc("/db/retrieve/all", getAll).Methods("GET")
-	router.HandleFunc("/db/retrieve/snippets/{amount}", newPage).Methods("GET")
-	router.HandleFunc("/db/retrieve/suggestions/{amount}", newPageWithSuggestions).Methods("GET")
+	router.HandleFunc("/db/retrieve/snippets/{amount}", newPageWithSuggestions).Methods("GET")
 	router.HandleFunc("/db/retrieve/recognizer/{amount}", newBatchForReco).Methods("GET")
 	router.HandleFunc("/db/status", status).Methods("GET")
 
